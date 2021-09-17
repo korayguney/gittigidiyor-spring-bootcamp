6ff871816e7610dd9c52cf628e2745c618d431c7
Koray Guney <koray.guney@hotmail.com>
2021-09-17 20:14:43 +0300

    updated patika-06-01

--- patika-06-01/src/test/java/dev/patika/patika0601/Patika0601ApplicationTests.java
+++ patika-06-01/src/test/java/dev/patika/patika0601/Patika0601ApplicationTests.java
@@ -1,5 +1,6 @@
 package dev.patika.patika0601;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class Patika0601ApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.fail("There is an ERROR !!!");
     }
 
 }
